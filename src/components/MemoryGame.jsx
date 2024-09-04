c6ba2e24bcd9d19d26da40a1fd489f61da4a29a4
Corina Udrescu <corinutz@gmail.com>
2024-09-04 08:07:22 +0200

    add sample images

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,8 +1,24 @@
-const MemoryGame = () => {
+const MemoryGame = ({ images }) => {
   return (
     <div>
       <h1>Memory Game</h1>
       <p>Build your memory game!</p>
+      <p>Here are the sample images you can use:</p>
+      <div>
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Memory card ${index}`}
+            style={{
+              width: "100px",
+              height: "100px",
+              margin: "5px",
+              objectFit: "cover",
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 };
