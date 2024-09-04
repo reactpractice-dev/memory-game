690a4d4514db99ce8de55ae7d02d570f6ca9cedf
Corina Udrescu <corinutz@gmail.com>
2024-09-04 08:56:09 +0200

    add image flipping

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,15 +1,72 @@
 import { useState } from "react";
 
 const createGameImages = (images) => {
-  const imagePairs = images.concat(images);
+  const imagePairs = [...images, ...images];
   const shuffledImagePairs = imagePairs.sort(() => Math.random() - 0.5);
-  return shuffledImagePairs;
+  return shuffledImagePairs.map((image, index) => ({
+    id: index,
+    src: image,
+    isTurned: false,
+    isMatched: false,
+  }));
 };
 
-const MemoryGame = ({ images: initialImage }) => {
-  const [shuffledImagePairs, setShuffledImagePairs] = useState(
-    createGameImages(initialImage)
-  );
+const MemoryGame = ({ images: initialImages }) => {
+  const [gameImages, setGameImages] = useState(createGameImages(initialImages));
+
+  const flipImage = (image) => {
+    setGameImages((prevImages) => {
+      const newImages = prevImages.map((prevImage) =>
+        prevImage.id === image.id ? { ...prevImage, isTurned: true } : prevImage
+      );
+      return newImages;
+    });
+  };
+
+  const markImagesAsMatched = (imageSource) => {
+    setGameImages((prevImages) => {
+      const newImages = prevImages.map((prevImage) =>
+        prevImage.src === imageSource
+          ? { ...prevImage, isMatched: true, isTurned: false }
+          : prevImage
+      );
+      return newImages;
+    });
+  };
+
+  const resetBoardAndFlipImage = (image) => {
+    setGameImages((prevImages) => {
+      const newImages = prevImages.map((prevImage) =>
+        prevImage.id === image.id
+          ? { ...prevImage, isTurned: true }
+          : { ...prevImage, isTurned: false }
+      );
+      return newImages;
+    });
+  };
+
+  const handleImageClick = (image) => {
+    if (image.isTurned || image.isMatched) {
+      // if user clicked an already visible image, do nothing
+      return;
+    }
+    const imagesAlreadyTurned = gameImages.filter((image) => image.isTurned);
+    if (imagesAlreadyTurned.length === 0) {
+      // new round: just turn image and wait for the user to click another image
+      flipImage(image);
+      return;
+    } else if (imagesAlreadyTurned.length === 1) {
+      if (imagesAlreadyTurned[0].src === image.src) {
+        // if user clicked the matching image, turn it around and keep it visible
+        markImagesAsMatched(image.src);
+      } else {
+        // we don't have a match: reset
+        flipImage(image);
+      }
+    } else {
+      resetBoardAndFlipImage(image);
+    }
+  };
 
   return (
     <div>
@@ -25,19 +82,32 @@ const MemoryGame = ({ images: initialImage }) => {
           width: "660px",
         }}
       >
-        {shuffledImagePairs.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Memory card ${index}`}
-            style={{
-              width: "100%",
-              height: "100%",
-              margin: "5px",
-              objectFit: "cover",
-            }}
-          />
-        ))}
+        {gameImages.map((image) =>
+          image.isTurned || image.isMatched ? (
+            <img
+              key={image.id}
+              src={image.src}
+              style={{
+                width: "100%",
+                height: "100%",
+                margin: "5px",
+                objectFit: "cover",
+              }}
+            />
+          ) : (
+            <div
+              key={image.id}
+              style={{
+                display: "block",
+                width: "100%",
+                height: "100%",
+                margin: "5px",
+                backgroundColor: "lightgray",
+              }}
+              onClick={() => handleImageClick(image)}
+            ></div>
+          )
+        )}
       </div>
     </div>
   );
